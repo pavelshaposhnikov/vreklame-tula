6270aba8734a891cb4ad771ebfde9baec34c28d8
pavelshaposhnikov <77579244+pavelshaposhnikov@users.noreply.github.com>
2021-03-06 22:05:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.portfolio__items').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: true,
+  });
+
+  $('.reviews__items').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: true,
+  });
